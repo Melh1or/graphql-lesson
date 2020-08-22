127366ddae7a67e866a1b9e484abd088bbf9f5c3
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-22 13:32:17 +0300

    Type Relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,17 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = require("graphql");
 
 const movies = [
-  { id: "1", name: "1984", genre: "Sci-Fi" },
-  { id: "2", name: "V", genre: "Sci-Fi-Thriller" },
-  { id: 3, name: "Snatch", genre: "Comedy" },
-  { id: 4, name: "One Out", genre: "Anime" },
+  { id: "1", name: "1984", genre: "Sci-Fi", directorId: "1" },
+  { id: "2", name: "V", genre: "Sci-Fi-Thriller", directorId: "2" },
+  { id: "3", name: "Snatch", genre: "Comedy", directorId: "3" },
+  { id: "4", name: "One Out", genre: "Anime", directorId: "4" },
+]
+
+const directors = [
+  { id: "1", name: "Serhii", age: 21 },
+  { id: "2", name: "Roman", age: 20 },
+  { id: "3", name: "Dri", age: 17 },
+  { id: "4", name: "Nikiiiitaaaa", age: 19 },
 ]
 
 const MovieType = new GraphQLObjectType({
@@ -13,6 +20,22 @@ const MovieType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find(director => director.id == parent.id)
+      }
+    }
+  })
+})
+
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+
   })
 })
 
@@ -21,9 +44,16 @@ const Query = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLID } },
+      args: {id: {type: GraphQLID}},
       resolve(parent, args) {
         return movies.find(movie => movie.id == args.id)
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return directors.find(director => director.id == args.id)
       }
     }
   }
