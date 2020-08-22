8a877d67e07dbc88a5e50e5b389e84ea91ef427e
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-22 12:29:20 +0300

    GraphQL Schema and Root Query

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 
+const schema = require("../schema/schema")
+
 const app = express();
-app.use("/graphql", graphqlHTTP({}));
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphiql: true
+}));
 
 const PORT = process.env.PORT || 3010;
 app.listen(PORT, (err) => {
