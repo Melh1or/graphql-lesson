b0fedcf5cb0e14f04da6bd370ed75272c77184db
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-22 13:17:22 +0300

    The resolve function

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,16 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = require("graphql");
+
+const movies = [
+  { id: "1", name: "1984", genre: "Sci-Fi" },
+  { id: "2", name: "V", genre: "Sci-Fi-Thriller" },
+  { id: 3, name: "Snatch", genre: "Comedy" },
+  { id: 4, name: "One Out", genre: "Anime" },
+]
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   })
@@ -14,9 +21,9 @@ const Query = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-
+        return movies.find(movie => movie.id == args.id)
       }
     }
   }
